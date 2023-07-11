3072700f6d787cd14cdda6c711e983bc351a789a
none_None1 <mxm20110612@163.com>
2023-07-11 09:48:42 +0800

    Update lstackg.js

--- lstackg.js
+++ lstackg.js
@@ -1,11 +1,11 @@
 function lstackg(code,input){
     var stack=[0],par=[];
     var match={},cp=0,output='';
-    for(let i of code){
-        if(i=='<'){
+    for(let i=0;i<code.length;i++){
+        if(code[i]=='<'){
             par.push(i);
         }
-        if(i=='>'){
+        if(code[i]=='>'){
             if(par.length==0){
                 return 'Unmatched >';
             }
@@ -27,6 +27,7 @@ function lstackg(code,input){
             }else{
                 cp=cp+1;
             }
+            continue;
         }
         if(code[cp]=='>'){
             if(stack[stack.length-1]!=0){
@@ -34,36 +35,43 @@ function lstackg(code,input){
             }else{
                 cp=cp+1;
             }
+            continue;
         }
         if(code[cp]=='a'){
             stack[stack.length-1]+=1;
+            stack[stack.length-1]%=256;
             cp=cp+1;
+            continue;
         }
         if(code[cp]=='s'){
             stack.push(0);
             cp=cp+1;
+            continue;
         }
         if(code[cp]=='t'){
             if(input.length==0){
-                stack[stack.length-1]=255;
+                stack.push(255);
             }else{
                 var inch=input.charCodeAt(0);
                 input=input.slice(1);
                 stack.push(inch);
             }
             cp=cp+1;
+            continue;
         }
         if(code[cp]=='c'){
             if(stack.length>=2){
                 output+=String.fromCharCode(stack.pop());
             }
             cp=cp+1;
+            continue;
         }
         if(code[cp]=='k'){
             if(stack.length>=2){
                 stack.pop();
             }
             cp=cp+1;
+            continue;
         }
     }
     return output
